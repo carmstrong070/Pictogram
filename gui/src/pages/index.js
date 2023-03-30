5d1def35e02038954aa081c05f9c65d82d38ee61
Alex Pallotta <pallotaw@gmail.com>
2023-03-29 18:14:11 -0700

    Refactored board component to accept the player's progress instead of the solution, added puzzleProgress state, added handlePuzzleChange function
    Refactored board component to accept the player's progress instead of the solution, added puzzleProgress state, added handlePuzzleChange function

--- gui/src/pages/index.js
+++ gui/src/pages/index.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react"
+import * as R from 'ramda'
 
 const index = () => {
   const [puzzle, setPuzzle] = useState()
+  const [puzzleProgress, setPuzzleProgress] = useState([])
 
+  // Initialize puzzle
   useEffect(() => {
     setPuzzle({
       "title": "Box",
@@ -21,32 +24,61 @@ const index = () => {
       ],
       "hint": "It's just a box"
     })
+    setPuzzleProgress(
+      [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      ]
+    )
   }, [])
 
-  const BorderClasses = (rowIndex, cellIndex) => {
-    var classes = "";
 
-    if(rowIndex === 0)
+  const handlePuzzleChange = (xIndex, yIndex) => {
+    let currentPuzzle = puzzleProgress
+    if (currentPuzzle[yIndex][xIndex] === 0) {
+      currentPuzzle[yIndex][xIndex] = 1
+    }
+    else if (currentPuzzle[yIndex][xIndex] === 1) {
+      currentPuzzle[yIndex][xIndex] = 0
+    }
+    setPuzzleProgress(currentPuzzle)
+    if (R.equals(puzzleProgress, puzzle.solution)) {
+      console.log("Puzzle finished!")
+    }
+  }
+
+  const BorderClasses = (rowIndex, cellIndex) => {
+    let classes = "";
+
+    if (rowIndex === 0)
       classes += "thick-border-top ";
 
-    if((rowIndex + 1) % 5 === 0)
+    if ((rowIndex + 1) % 5 === 0)
       classes += "thick-border-bottom ";
 
-    if((cellIndex + 1) % 5 === 0)
+    if ((cellIndex + 1) % 5 === 0)
       classes += "thick-border-right ";
 
     return classes;
   };
 
   const Board = ({ puzzle }) => {
-    if (puzzle) {
+    if (puzzle[0] !== undefined) {
       return (
         <table>
           <thead>
             <tr>
               <th>😐</th>
               {/* Create a header row for each column that will contain the guide numbers*/}
-              {puzzle.solution[0].map((head, headIndex) => {
+              {puzzle[0].map((head, headIndex) => {
                 return (
                   <th key={headIndex}>{headIndex + 1}</th>
                 )
@@ -55,7 +87,7 @@ const index = () => {
           </thead>
           <tbody>
             {/* Create a table row for each array in the solution array */}
-            {puzzle.solution.map((row, rowIndex) => {
+            {puzzle.map((row, rowIndex) => {
               return (
                 <tr row={rowIndex + 1} key={rowIndex}>
                   {row.map((cell, cellIndex) => {
@@ -63,15 +95,13 @@ const index = () => {
                     if (cellIndex == 0) {
                       return ([
                         <React.Fragment key={rowIndex}>
-                          <td key={`${rowIndex}`}>{cellIndex}</td>
-                          <td className={`cell thick-border-left ${BorderClasses(rowIndex, cellIndex)}`} key={`${rowIndex} ${cellIndex}`} y={cellIndex} x={rowIndex}>{cell ? "X" : " "}</td>
+                          <td key={`${rowIndex}`}>{rowIndex + 1}</td>
+                          <td className={`cell thick-border-left ${BorderClasses(rowIndex, cellIndex)}`} key={`${rowIndex} ${cellIndex}`} y-index={cellIndex} x-index={rowIndex} onClick={() => handlePuzzleChange(cellIndex, rowIndex)}></td>
                         </React.Fragment>
                       ])
                     }
                     else return (
-                      <td className={`cell ${BorderClasses(rowIndex, cellIndex)}`} key={`${rowIndex} ${cellIndex}`} y={cellIndex} x={rowIndex}>
-                        {cell ? "X" : " "}
-                      </td>
+                      <td className={`cell ${BorderClasses(rowIndex, cellIndex)}`} key={`${rowIndex} ${cellIndex}`} y-index={cellIndex} x-index={rowIndex} onClick={() => handlePuzzleChange(cellIndex, rowIndex)}></td>
                     )
                   })}
                 </tr>
@@ -87,7 +117,7 @@ const index = () => {
 
   return (
     <>
-      <Board puzzle={puzzle} />
+      <Board puzzle={puzzleProgress} />
     </>
   )
 }
