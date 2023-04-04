922f571c0f5154f705419d880999e31d2e15a51d
Alex Pallotta <pallotaw@gmail.com>
2023-04-04 10:36:24 -0700

    Refined click and drag logic inside of handleMouseUp, removed Nate

--- gui/src/pages/index.js
+++ gui/src/pages/index.js
@@ -17,6 +17,34 @@ const index = () => {
     }
   }, [puzzleProgress])
 
+
+  const handleClick = (button, startValue, endValue, currentPuzzle, columnIndex, rowIndex) => {
+    let currentCell = currentPuzzle[rowIndex][columnIndex]
+
+    // Left click logic
+    if (button === 0) {
+      if (currentCell === 0 && startValue !== 1 || startValue === 0 && endValue === 1 && currentCell !== 2) {
+        currentPuzzle[rowIndex][columnIndex] = 1
+      }
+      else if (endValue === 1 && currentCell !== 2 || startValue === 1 && currentCell === 1) {
+        currentPuzzle[rowIndex][columnIndex] = 0
+      }
+    }
+
+    // Right click logic
+    else if (button === 2) {
+      if (startValue === 0 && currentCell !== 1 || startValue === 0 && endValue === 0 && currentCell !== 1) {
+        currentPuzzle[rowIndex][columnIndex] = 2
+      }
+      else if (startValue === 1 && endValue !== 2 && currentCell === 1 || startValue === 2 && endValue === 0 && currentCell !== 1) {
+        currentPuzzle[rowIndex][columnIndex] = 0
+      }
+      else if (startValue === 2 && endValue === 2 && currentCell === 2 || startValue === 1 && endValue === 1 && currentCell === 1) {
+        currentPuzzle[rowIndex][columnIndex] = 0
+      }
+    }
+  }
+
   const handleMouseDown = (e, value, columnIndex, rowIndex) => {
     e.preventDefault()
     setMouseDownPosition({
@@ -24,43 +52,6 @@ const index = () => {
       row: rowIndex,
       initialValue: value
     })
-  }
-
-  const handleClick = (button, startValue, endValue, currentPuzzle, columnIndex, rowIndex) => {
-    let currentCell = currentPuzzle[rowIndex][columnIndex]
-
-    // Left click logic
-    if (button === 0) {
-      if (currentCell === 0) {
-        currentPuzzle[rowIndex][columnIndex] = 1
-      }
-      else if (startValue === 0 && endValue === 1 && currentCell !== 2) {
-        currentPuzzle[rowIndex][columnIndex] = 1
-      }
-      else if (endValue === 1 && currentCell !== 2) {
-        currentPuzzle[rowIndex][columnIndex] = 0
-      }
-    }
-
-    // Right click logic
-    else if (button === 2) {
-      if (startValue === 0 && endValue === 0 && currentCell !== 1) {
-        currentPuzzle[rowIndex][columnIndex] = 2
-        console.log("1st case")
-      }
-      else if (startValue === 0 && endValue === 0 && currentCell === 1) {
-        currentPuzzle[rowIndex][columnIndex] = 1
-        console.log("2nd case")
-      }
-      else if (startValue === 0 && endValue === 2 && currentCell !== 1 || startValue === 0 && endValue === 2 && currentCell !== 1) {
-        currentPuzzle[rowIndex][columnIndex] = 2
-        console.log("3rd case")
-      }
-      else if (currentCell === 2 && startValue !== 1 && endValue !== 1 || currentCell === 1) {
-        currentPuzzle[rowIndex][columnIndex] = 0
-        console.log("4th case")
-      }
-    }
   }
 
   const handleMouseUp = (e, value, columnIndex, rowIndex) => {
