e45841d78e91712e7a8323e90ef4e924918da9af
Christian Armstrong <c.armstrong070@gmail.com>
2023-04-05 15:25:48 -0700

    Timer play and pause functionality

---
+++ gui/src/helpers/timerHelpers.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faPlay} from "@fortawesome/free-solid-svg-icons";
+import {faPause} from "@fortawesome/free-solid-svg-icons";
+
+
+export const togglePlayPause = (timerIcon, setTimerIcon, setTimerIsActive) => {
+  if (timerIcon == faPlay){
+    setTimerIcon(faPause);
+    setTimerIsActive(true);
+  }
+  else{
+    setTimerIcon(faPlay);
+    setTimerIsActive(false);
+  }
+}
+
+export default { togglePlayPause }
--- gui/src/pages/index.js
+++ gui/src/pages/index.js
@@ -4,10 +4,16 @@ import Timecode from 'react-timecode';
 import * as puzzleHelpers from "../helpers/puzzleHelpers"
 import PuzzleSelection from "@/components/PuzzleSelection"
 import Board from "@/components/Board"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faPlay} from "@fortawesome/free-solid-svg-icons";
+import {faPause} from "@fortawesome/free-solid-svg-icons";
+import { togglePlayPause } from "@/helpers/timerHelpers";
 
 const index = () => {
   const [puzzle, setPuzzle] = useState()
   const [puzzleProgress, setPuzzleProgress] = useState([])
+  const [timerIcon, setTimerIcon] = useState(faPlay)
+  const [timerIsActive, setTimerIsActive] = useState(false)
 
   useEffect(() => {
     if (puzzle && puzzleProgress) {
@@ -21,9 +27,10 @@ const index = () => {
   return (
     <>
       <div className="timer-wrapper">
-        <Timer className="timer" duration={null}>
+        <Timer className="timer" active={timerIsActive} duration={null}>
           <Timecode />
         </Timer>
+        <button onClick={e => togglePlayPause(timerIcon, setTimerIcon, setTimerIsActive)} type="button" className="timer-button"><FontAwesomeIcon icon={timerIcon} /></button>
       </div>
       {puzzle && puzzleProgress ? <Board puzzleProgress={puzzleProgress} setPuzzleProgress={setPuzzleProgress} puzzleSolution={puzzle.solution} /> : <></>}
       <br />
