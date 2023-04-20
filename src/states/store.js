41683f8bc8d5655337fea9eb4419bbcbd6f0b6f9
Alex Pallotta <pallotaw@gmail.com>
2023-04-20 09:38:38 -0700

    Fixed Box puzzle, fixed gameStore syntax

--- src/states/store.js
+++ src/states/store.js
@@ -3,19 +3,19 @@ import { mountStoreDevtool } from "simple-zustand-devtools";
 
 const gameStore = create((set) => ({
   userDifficulty: 0,
-  setDifficulty: (int) => set(() => ({ userDifficulty: int })),
+  setDifficulty: (int) => set({ userDifficulty: int }),
   isFinished: false,
-  setIsFinished: (bool) => set(() => ({ isFinished: bool })),
+  setIsFinished: (bool) => set({ isFinished: bool }),
   cursorPosition: undefined,
-  setCursorPosition: (obj) => set(() => ({ cursorPosition: obj })),
+  setCursorPosition: (obj) => set({ cursorPosition: obj }),
   puzzleProgress: [],
-  setPuzzleProgress: (arr) => set(() => ({ puzzleProgress: arr })),
+  setPuzzleProgress: (arr) => set({ puzzleProgress: arr }),
   puzzle: {},
-  setPuzzle: (obj) => set(() => ({ puzzle: obj })),
+  setPuzzle: (obj) => set({ puzzle: obj }),
   time: 0,
-  setTime: (int) => set(() => ({ time: int })),
+  setTime: (int) => set({ time: int }),
   running: true,
-  setRunning: (bool) => set(() => ({ running: bool })),
+  setRunning: (bool) => set({ running: bool }),
   mouseDownInfo: {
     column: undefined,
     row: undefined,
@@ -23,13 +23,13 @@ const gameStore = create((set) => ({
     button: undefined,
     isDragging: false,
   },
-  setMouseDownInfo: (obj) => set(() => ({ mouseDownInfo: obj })),
+  setMouseDownInfo: (obj) => set({ mouseDownInfo: obj }),
   timerStatus: {
     reset: false,
     stopped: false,
     expired: false,
   },
-  setTimerStatus: (obj) => set(() => ({ timerStatus: obj })),
+  setTimerStatus: (obj) => set({ timerStatus: obj }),
 }));
 
 if (process.env.NODE_ENV === "development") {
